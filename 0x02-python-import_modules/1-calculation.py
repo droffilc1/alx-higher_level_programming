a0b09ca383738063eb19cb17fa79e19543bf8032
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-10-05 18:27:33 +0300

    Update solution to task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,12 +5,13 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    result1 = add(a, b)
-    result2 = sub(a, b)
-    result3 = mul(a, b)
-    result4 = div(a, b)
+    sum_result = add(a, b)
+    sub_result = sub(a, b)
+    mul_result = mul(a, b)
+    div_result = div(a, b)
 
-    print("{} + {} = {}".format(a, b, result1))
-    print("{} + {} = {}".format(a, b, result2))
-    print("{} + {} = {}".format(a, b, result3))
-    print("{} + {} = {}".format(a, b, result4))
+    print("{} + {} = {}".format(a, b, sum_result))
+    print("{} - {} = {}".format(a, b, sub_result))
+    print("{} * {} = {}".format(a, b, mul_result))
+    print("{} / {} = {}".format(a, b, div_result))
+
