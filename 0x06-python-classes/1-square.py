3e672fd6e0d19275cdacf1dd0ac5d6d3a77b8791
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-10-24 14:16:25 +0300

    feat: add solution to task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A module of class Square"""
+
+class Square:
+    """Represents a Square"""
+    def __init__(self, __self):
+        """Initializes the data"""
+        self.__self = __self
