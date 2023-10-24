c9990f79adafe829f5a4b4f893d734c197a76904
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-10-24 14:28:39 +0300

    fix: pycodestyle to task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """A module of class Square"""
 
+
 class Square:
     """Represents a Square"""
     def __init__(self, size):
         """Initializes the instance
-        
         Args:
             size: Defines instance attribute
         """
