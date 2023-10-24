f15d614736bea9c543e5ad66ff85ca83d15fccd7
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-10-24 15:30:59 +0300

    feat: add solution to task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A module of class Square"""
+
+
+class Square:
+    """Represents a Square"""
+    def __init__(self, size=0):
+        """Initializes the instance
+        Args:
+            size: Defines instance attribute
+        """
+        if isinstance(size, int):
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Public instance method that returns the current square"""
+        return self.__size * self.__size
