70706aacaede02b4af68dd450fbd76e81d82fdd8
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-10-24 18:30:41 +0300

    fix: update solution to task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -45,10 +45,10 @@ class Square:
         the square with the character #:
         if size is equal to 0, print an empty line
         """
-        for i in range(self.__size):
-            for j in range(self.__size):
-                if self.__size == 0:
-                    print()
-                else:
-                    print("#", end="")
+        if self.__size == 0:
             print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
