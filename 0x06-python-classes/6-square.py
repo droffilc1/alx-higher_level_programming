66e3cd1c66091ecef99086b89297b48a471d8484
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-10-25 08:59:04 +0300

    fix: update solution to task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,6 +48,7 @@ class Square:
            not all(isinstance(i, int) and i >= 0 for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
+
     def my_print(self):
         """Public instance method that prints in stdout
         the square with the character #:
