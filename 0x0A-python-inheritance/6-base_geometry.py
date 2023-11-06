0fa4961387606841f16cca2e86c7ce96782a3379
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-11-06 20:16:10 +0300

    feat: add solution to task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A module for a BaseGeometry class."""
+
+
+class BaseGeometry:
+    """A simple BaseGeometry class.
+
+    Raises:
+        Exception: area() is not implemented.
+    """
+    def area(self):
+        """Calculates the area."""
+        raise Exception("area() is not implemented.")
