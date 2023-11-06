cd7d28a2f2f6e3eab4177543d62e9a2ee0a11f53
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-11-06 22:30:02 +0300

    feat: add solution to task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A module for a Square class."""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A Square class."""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
