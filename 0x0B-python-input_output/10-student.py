845f5a6dab439bf06dbc5734dd0a6ac482d71d7b
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-11-07 16:06:01 +0300

    feat: add solution to task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A class Student module."""
+
+
+class Student:
+    """A Student class."""
+    def __init__(self, first_name, last_name, age):
+        """Initializing data."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary represenation of a Student instance."""
+        if attrs is None:
+            return self.__dict__
+        return {key: self.__dict__[key]
+                for key in attrs if key in self.__dict__}
