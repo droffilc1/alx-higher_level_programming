9b77fce55ca39f450ce037dc874ecad739284270
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-11-09 15:59:33 +0300

    feat: validate attributes

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -19,5 +19,23 @@ class TestRectangleClass(unittest.TestCase):
         r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r3.id, 12)
 
+    def test_types(self):
+        """Test for different types"""
+        with self.assertRaises(TypeError,
+                               msg="height must be an integer"):
+            Rectangle(10, "2")
+        with self.assertRaises(TypeError, 
+                               msg="width must be an integer"):
+            Rectangle("10", 2)
+        with self.assertRaises(ValueError, 
+                               msg="width must be > 0"):
+            Rectangle(-10, 2)
+        with self.assertRaises(TypeError, 
+                                msg="x must be an integer"):
+            Rectangle(10, 2, [], 3)
+        with self.assertRaises(ValueError, 
+                               msg="y must be >= 0"):
+            Rectangle(10, 2, 3, -1)
+
 if __name__ == "__main__":
     unittest.main()
