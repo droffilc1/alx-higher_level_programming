4c5873858095142004e67d8da81d29570ec6c490
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-11-13 14:39:18 +0300

    feat: square update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -107,15 +107,9 @@ class Rectangle(Base):
     def update(self, *args, **kwargs):
         """Assigns an argument to class attributes"""
         if args:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.__width = args[1]
-            if len(args) >= 3:
-                self.__height = args[2]
-            if len(args) >= 4:
-                self.__x = args[3]
-            if len(args) >= 5:
-                self.__y = args[4]
+            attrs = ["id", "size", "x", "y"]
+            for i, value in enumerate(args[:4]):
+                setattr(self, attrs[i], args[i])
+
         for key, value in kwargs.items():
             setattr(self, key, value)
