95e81b4a75182594daf9fec98134167d27bf6c2c
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-12-15 21:01:52 +0300

    fix: Best genre

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -3,7 +3,7 @@
 -- Each record should display: tv_genres.name - rating sum
 -- Results must be sorted in descending order by their rating
 -- You can use only one SELECT statement
-SELECT tv_genres.name, SUM(tv_show_ratings.rate)
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
