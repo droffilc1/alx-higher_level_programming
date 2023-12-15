45f93001bc817d89c471159ac548d79196a527a7
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-12-15 17:57:37 +0300

    fix: cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,7 +5,8 @@
 -- name VARCHAR(256) can’t be nul
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    state_id INT,
+    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
     name VARCHAR(256) NOT NULL
 );
