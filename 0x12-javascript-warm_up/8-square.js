a84736b693ae70a70ef13b9d137303c4096281f9
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-01-08 10:58:22 +0300

    feat: square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const size = parseInt(process.argv[2], 10);
+let count = 0;
+
+if (isNaN(size)) {
+    console.log('Missing size');
+} else {
+    for (; count < size; count++) {
+        console.log('X'.repeat(size));
+    }
+}
