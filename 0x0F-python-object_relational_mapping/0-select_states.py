8e60d60ccb8342ddd24983b8f973d9e05b1a814d
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-01-11 12:18:01 +0300

    feat: get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+    0-select_states
+"""
+import os
+import MySQLdb
+from dotenv import load_dotenv
+
+
+# Load environment variables
+load_dotenv()
+
+MY_HOST = os.getenv("MY_HOST")
+MY_USER = os.getenv("MY_USER")
+MY_PASS = os.getenv("MY_PASS")
+MY_DB = os.getenv("MY_DB")
+
+def main():
+    """
+    Entry point
+    """
+    # Connect to a MySQL database
+    db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC LIMIT 5")
+
+    # Fetch all rows
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close cursir and database connection
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    main()
