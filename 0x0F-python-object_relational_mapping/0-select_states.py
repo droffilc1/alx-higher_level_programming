63b53d2710114604cd460ce3d3a97b6851dc382e
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-01-12 11:10:58 +0300

    fix: get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,18 +2,8 @@
 """
     0-select_states
 """
-import os
+from sys import argv
 import MySQLdb
-from dotenv import load_dotenv
-
-
-# Load environment variables
-load_dotenv()
-
-MY_HOST = os.getenv("MY_HOST")
-MY_USER = os.getenv("MY_USER")
-MY_PASS = os.getenv("MY_PASS")
-MY_DB = os.getenv("MY_DB")
 
 
 def main():
@@ -21,7 +11,11 @@ def main():
     Entry point
     """
     # Connect to a MySQL database
-    db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC LIMIT 5")
 
