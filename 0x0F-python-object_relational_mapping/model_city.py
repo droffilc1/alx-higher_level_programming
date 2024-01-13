01a86d4e519063268cd9a28554891626f559401c
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-01-13 13:03:54 +0300

    style: whitespace

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -23,4 +23,4 @@ class City(Base):
     __tablename__ = "cities"
     id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("state.id") , nullable=False)
+    state_id = Column(Integer, ForeignKey("state.id"), nullable=False)
