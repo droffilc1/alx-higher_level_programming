68b0da835d0178190a6281b65d4f03be30d63f5e
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-02-20 12:03:19 +0300

    feat: star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// Prints the number of movies where the character “Wedge Antilles” is present.
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const results = JSON.parse(body).results;
+    let count = 0;
+    for (const i in results) {
+      const chars = results[i].characters;
+      for (const c in chars) {
+        if (chars[c].includes('18')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
